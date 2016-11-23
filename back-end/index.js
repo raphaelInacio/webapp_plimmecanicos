316cb0142d021d602016bc8cc9422b7c2de41717
raphaelInacio <contato.raphaelinacio@gmail.com>
2016-11-23 21:57:23 -0200

    implementaçõa aviso de usuario logado chat

--- back-end/index.js
+++ back-end/index.js
@@ -7,9 +7,24 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-   io.emit('chat message', msg);
+
+  socket.on('chat', function(data){
+   io.emit('chat', data);
   });
+
+  socket.on('login', function(data){
+    data.mensagem = 'usuario logado'
+    io.emit('login', data)
+  })
+
+   socket.on('load', function(data){
+    data.mensagem = 'usuario na sala de chat'
+    io.emit('chat', data)
+  })
+
+  socket.on('disconnect', function(data) {
+      io.emit('disconnect', data)
+		});
 });
 
 http.listen(3000, function(){
--- front-end/app/chat/chat.controller.js
+++ front-end/app/chat/chat.controller.js
@@ -7,52 +7,80 @@ function ChatController($http) {
     vm = this
     vm.socket = io('http://localhost:3000/');
     vm.mensagem = ''
-    vm.msgChat = ''
-    vm.user = 'Raphael'
-    vm.obterDataMensagem = 
-        function obterDataMensagem(){
-            var data = new Date();
-            
-            var dia     = data.getDate();           // 1-31
-            var dia_sem = data.getDay();            // 0-6 (zero=domingo)
-            var mes     = data.getMonth();          // 0-11 (zero=janeiro)
-            var ano2    = data.getYear();           // 2 dígitos
-            var ano4    = data.getFullYear();       // 4 dígitos
-            var hora    = data.getHours();          // 0-23
-            var min     = data.getMinutes();        // 0-59
-            var seg     = data.getSeconds();        // 0-59
-            var mseg    = data.getMilliseconds();   // 0-999
-            var tz      = data.getTimezoneOffset(); // em minutos
 
-            // Formata a data e a hora (note o mês + 1)
-            var str_data = dia + '/' + (mes+1) + '/' + ano4;
-            var str_hora = hora + ':' + min + ':' + seg;
-
-            var data =  str_data + ' ' + str_hora 
-            return data
+    vm.obterDataMensagem =
+        function obterDataMensagem() {
+            var data = new Date().toISOString().split('T')[0].split('-').reverse().toString().replace(/,/g, '-')
+            var hora = new Date().toISOString().split('T')[1].split('.')[0].replace()
+            return data + ':' + hora
         }
+
+    vm.data = {
+        id: Math.round((Math.random() * 1000000)),
+        user: 'Mecanica ' + Math.round((Math.random() * 1000000)),
+        mensagem: '',
+        dataHora: vm.obterDataMensagem()
+    }
+
     vm.enviarMensagem =
         function enviarMensagem() {
-            vm.socket.emit('chat message', vm.mensagem);
-            vm.mensagem = ''
+            vm.data.mensagem = vm.mensagem
+            vm.data.dataHora = vm.obterDataMensagem()
+            vm.socket.emit('chat', vm.data);
+            montarMensagemRemetente()
         }
 
-    vm.socket.on('chat message', function (msg) {
-
-    $('#messages').append( '<dd class="pos-left clearfix">' +
-        '<div class="circ"></div>' +
-        '<div class="time">'+ vm.obterDataMensagem() + '</div>' +
-        '<div class="events">' +
+    function montarMensagemRemetente() {
+        $('#messages').append('<dd class="pos-left clearfix">' +
+            '<div class="circ"></div>' +
+            '<div class="time">' + vm.data.dataHora + '</div>' +
+            '<div class="events">' +
             '<div class="pull-left">' +
-                '<img class="events-object img-rounded" src="http://bootflat.github.io/img/photo-2.jpg">' +
+            '<img class="events-object img-rounded" src="../../imagens/unnamed.jpg">' +
             '</div>' +
             '<div class="events-body">' +
-                '<h4 class="events-heading">'+ vm.user +'</h4>' +
-                '<p>'+ msg + '</p>' +
+            '<h4 class="events-heading">' + vm.data.user + '</h4>' +
+            '<p>' + vm.data.mensagem + '</p>' +
             '</div>' +
-        '</div>' +
-    '</dd>' );
+            '</div>' +
+            '</dd>');
+        vm.mensagem = ''
+    }
 
-   
+    vm.socket.on('chat', function (data) {
+        if (data.id !== vm.data.id) {
+            montarMensagemDestinatario(data)
+        }
     });
+
+    vm.socket.on('load', function (data) {
+        montarMensagemDestinatario(data)
+    });
+
+    vm.socket.on('login', function (data) {
+        montarMensagemDestinatario(data)
+    });
+
+    //socket.on('disconnect', function(data) {
+    //   montarMensagemDestinatario(data)
+    //});
+
+    //vm.socket.emit('disconnect', vm.data);
+    vm.socket.emit('login', vm.data);
+
+    function montarMensagemDestinatario(data) {
+        $('#messages').append('<dd class="pos-right clearfix">' +
+            '<div class="circ"></div>' +
+            '<div class="time">' + data.dataHora + '</div>' +
+            '<div class="events">' +
+            '<div class="pull-left">' +
+            '<img class="events-object img-rounded" src="../../imagens/unnamed.jpg">' +
+            '</div>' +
+            '<div class="events-body">' +
+            '<h4 class="events-heading">' + data.user + '</h4>' +
+            '<p>' + data.mensagem + '</p>' +
+            '</div>' +
+            '</div>' +
+            '</dd>');
+    }
 }
